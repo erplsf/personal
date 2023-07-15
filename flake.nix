2f3d6c96d2d38f40badc96fe0e795ddc5acd3181
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-07-15 18:04:09 +0200

    add nix files

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Personal webpage development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # needed for shell shim: see NixOS Flakes wiki page
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          nodejs
+          nodePackages.typescript-language-server
+          nodePackages.pnpm
+        ];
+      };
+    };
+}
